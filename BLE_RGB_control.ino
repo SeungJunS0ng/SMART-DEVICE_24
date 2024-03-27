3ce5f950e008f904eece3723032f0d97ca3b5c68
Seung Jun Song <91440432+john020202@users.noreply.github.com>
2024-03-27 18:44:41 +0900

    Add files via upload

---
+++ BLE_RGB_control.ino
@@ -0,0 +1,46 @@
+#define RED_PIN 4   // 빨간색 핀
+#define GREEN_PIN 15 // 초록색 핀
+#define BLUE_PIN 2  // 파란색 핀
+
+void setup() {
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  
+  // 모든 핀을 HIGH로 설정하여 LED를 꺼진 상태로 초기화합니다.
+  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(GREEN_PIN, HIGH);
+  digitalWrite(BLUE_PIN, HIGH);
+}
+
+void loop() {
+  // 빨강
+  analogWrite(RED_PIN, 0);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 255);
+  delay(1000);
+
+  // 초록
+  analogWrite(RED_PIN, 255);
+  analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 255);
+  delay(1000);
+
+  // 파랑
+  analogWrite(RED_PIN, 255);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 0);
+  delay(1000);
+
+  // 백색
+  analogWrite(RED_PIN, 0);
+  analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 0);
+  delay(1000);
+
+  // 끄기
+  analogWrite(RED_PIN, 255);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 255);
+  delay(1000);
+}
